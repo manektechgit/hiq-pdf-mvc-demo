bf37c49c416ff61a028f6ed30967a459a53c1a9c
ShivShankar A Namdev <91069210+shiv-manektech@users.noreply.github.com>
2022-03-01 14:06:27 +0530

    first commit

---
+++ HiqpdfDemo/Controllers/ConvertHtmlToPdfController.cs
@@ -0,0 +1,43 @@
+﻿using HiQPdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HiqpdfDemo.Controllers
+{
+    public class ConvertHtmlToPdfController : Controller
+    {
+        // GET: ConvertHtmlToPdf
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult ConvertToPdf(FormCollection collection)
+        {
+            // create the HTML to PDF converter
+            HtmlToPdf htmlToPdfConverter = new HtmlToPdf();
+
+            // set PDF page size and orientation
+            htmlToPdfConverter.Document.PageSize = PdfPageSize.A4;
+            htmlToPdfConverter.Document.PageOrientation = PdfPageOrientation.Portrait;
+
+            // set PDF page margins
+            htmlToPdfConverter.Document.Margins = new PdfMargins(0);
+
+            // convert HTML to PDF
+            byte[] pdfBuffer = null;
+            string htmlCode = collection["htmlText"];
+            pdfBuffer = htmlToPdfConverter.ConvertHtmlToMemory(htmlCode, null);
+
+            FileResult fileResult = new FileContentResult(pdfBuffer, "application/pdf");            
+            fileResult.FileDownloadName = "HtmlToPdf.pdf";
+            return fileResult;
+        }
+
+    }
+}
---
+++ HiqpdfDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HiqpdfDemo.MvcApplication" Language="C#" %>
